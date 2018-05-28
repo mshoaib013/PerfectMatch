99195698b135b6a8c30c2b6d3ee93592b3a3b80b
darkshadow <shoaib.mehedi@gmail.com>
2018-05-29 03:44:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/darkshadow/perfectmatch/Welcome.java
@@ -0,0 +1,17 @@
+package com.example.darkshadow.perfectmatch;
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+public class Welcome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
